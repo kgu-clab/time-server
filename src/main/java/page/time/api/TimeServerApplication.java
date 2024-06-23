ce81af210ed67e12df32d4255c27529785f127d0
mingmingmon <mingmingmon@naver.com>
2024-06-23 16:54:05 +0900

    environment: 작업환경 세팅

---
+++ src/main/java/page/time/api/TimeServerApplication.java
@@ -0,0 +1,13 @@
+package page.time.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/page/time/api/TimeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package page.time.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

13bcb2718b414f86d25966e5a7d5e50f70c6055d
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-08-02 10:35:16 +0900

    refactor(SwaggerConfig): 필요 없는 import문 제거

--- src/main/java/page/time/api/global/config/SwaggerConfig.java
+++ src/main/java/page/time/api/global/config/SwaggerConfig.java
@@ -1,16 +1,11 @@
 package page.time.api.global.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 

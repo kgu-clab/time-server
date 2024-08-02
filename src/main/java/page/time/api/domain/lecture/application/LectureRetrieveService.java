fcdb424c570beb9e63f4b530e9d9457b17d77e25
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-08-02 15:18:11 +0900

    feat(Lecture): hasPrevious 구현

--- src/main/java/page/time/api/domain/lecture/application/LectureRetrieveService.java
+++ src/main/java/page/time/api/domain/lecture/application/LectureRetrieveService.java
@@ -1,7 +1,6 @@
 package page.time.api.domain.lecture.application;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import page.time.api.domain.lecture.dao.LectureRepository;
 import page.time.api.domain.lecture.domain.Lecture;
@@ -19,20 +18,28 @@ public class LectureRetrieveService {
     private final LectureRepository lectureRepository;
 
     public CursorResult<LectureResponseDto> retrieveLectures(
-            String campus, Type type, Integer grade, List<String> day, List<String> time, String major, Boolean isExceeded, String lectureName, Long cursor, Pageable pageable
+            String campus, Type type, Integer grade, List<String> day, List<String> time, String major, Boolean isExceeded, String lectureName, Long cursor, int limit
     ) {
-        List<Lecture> lectures = lectureRepository.findByFilter(campus, type, grade, day, time, major, isExceeded, lectureName, cursor, pageable);
+        List<Lecture> lectures = lectureRepository.findByFilter(campus, type, grade, day, time, major, isExceeded, lectureName, cursor, limit);
         List<LectureResponseDto> lectureDetailResponseDtos = lectures.stream()
                 .map(LectureResponseDto::toDto)
                 .collect(Collectors.toList());
-        return new CursorResult<>(lectureDetailResponseDtos, checkLastPage(pageable, lectureDetailResponseDtos));
+        return new CursorResult<>(lectureDetailResponseDtos, checkFirstPage(cursor, lectureDetailResponseDtos), checkLastPage(limit, lectureDetailResponseDtos));
     }
 
-    private boolean checkLastPage(Pageable pageable, List<LectureResponseDto> lectureDetailResponseDtos) {
+    private boolean checkFirstPage(Long cursor, List<LectureResponseDto> lectureDetailResponseDtos) {
+        boolean hasPrevious = false;
+        if ((cursor != null) && (lectureDetailResponseDtos.get(0).getId() > cursor)) {
+            hasPrevious = true;
+        }
+        return hasPrevious;
+    }
+
+    private boolean checkLastPage(int limit, List<LectureResponseDto> lectureDetailResponseDtos) {
         boolean hasNext = false;
-        if (lectureDetailResponseDtos.size() > pageable.getPageSize()) {
+        if (lectureDetailResponseDtos.size() > limit) {
             hasNext = true;
-            lectureDetailResponseDtos.remove(pageable.getPageSize());
+            lectureDetailResponseDtos.remove(limit);
         }
         return hasNext;
     }
--- src/main/java/page/time/api/domain/lecture/dao/LectureRepositoryCustomImpl.java
+++ src/main/java/page/time/api/domain/lecture/dao/LectureRepositoryCustomImpl.java
@@ -4,7 +4,6 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import page.time.api.domain.lecture.domain.Lecture;
 import page.time.api.domain.lecture.domain.Type;
@@ -20,7 +19,7 @@ public class LectureRepositoryCustomImpl implements LectureRepositoryCustom{
     private final JPAQueryFactory query;
 
     @Override
-    public List<Lecture> findByFilter(String campus, Type type, Integer grade, List<String> day, List<String> time, String major, Boolean isExceeded, String lectureName, Long cursor, Pageable pageable) {
+    public List<Lecture> findByFilter(String campus, Type type, Integer grade, List<String> day, List<String> time, String major, Boolean isExceeded, String lectureName, Long cursor, int limit) {
         return query
                 .selectFrom(lecture)
                 .where(
@@ -34,7 +33,7 @@ public class LectureRepositoryCustomImpl implements LectureRepositoryCustom{
                         containsToLectureName(lectureName),
                         gtCursor(cursor)
                 )
-                .limit(pageable.getPageSize() + 1)
+                .limit(limit + 1)
                 .fetch();
     }
 
@@ -59,25 +58,25 @@ public class LectureRepositoryCustomImpl implements LectureRepositoryCustom{
         return lecture.grade.eq(grade);
     }
 
-    private BooleanBuilder eqToDay(List<String> day) {
-        if (day == null || day.isEmpty()) {
+    private BooleanBuilder eqToDay(List<String> days) {
+        if (days == null || days.isEmpty()) {
             return null;
         }
         BooleanExpression isElearning = lecture.time.eq("이러닝").and(lecture.groupName.contains("이러닝"));
         BooleanBuilder builder = new BooleanBuilder(isElearning);
-        day.stream()
-                .map(d -> lecture.time.startsWith(d)
+        days.stream()
+                .map(day -> lecture.time.startsWith(day)
                         .and(lecture.groupName.contains("이러닝").not()))
                 .forEach(builder::or);
         return builder;
     }
 
-    private BooleanBuilder eqToTime(List<String> time) {
-        if (time == null || time.isEmpty()) {
+    private BooleanBuilder eqToTime(List<String> times) {
+        if (times == null || times.isEmpty()) {
             return null;
         }
         BooleanBuilder builder = new BooleanBuilder();
-        time.stream()
+        times.stream()
                 .map(lecture.time::contains)
                 .forEach(builder::or);
         return builder;
